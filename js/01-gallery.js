784b4af236769e952b02c751c6ade787bb9a1a79
Саша Маслак <owmaslak@gmail.com>
2022-06-04 09:47:19 +0300

    fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,31 +4,6 @@ const galeryContainer = document.querySelector('.gallery');
 const imagesMarkUp = createGalleryMarkup(galleryItems);
 galeryContainer.insertAdjacentHTML('beforeend', imagesMarkUp);
 
-galeryContainer.addEventListener('click', onGalleryClick);
-
-
-function onGalleryClick(e) {
-   e.preventDefault();
-   const hasImageClass = e.target.classList.contains('gallery__image');
-   if (!hasImageClass) {
-      return;
-   }
-   const instance = basicLightbox.create(
-      `
-      <img src="${e.target.dataset.source}" width="800" height="600">
-      `
-   );
-   instance.show();
-   window.addEventListener('keydown', e => onEscUp(e, instance));
-};
-
-function onEscUp(e, instance) {
-   if (e.code === "Escape") {
-      instance.close();
-      window.removeEventListener('keydown', onEscUp);
-   };
-};
-
 function createGalleryMarkup(galleryItems) {
    return galleryItems
       .map(({ preview, original, description }) => {
@@ -47,3 +22,40 @@ function createGalleryMarkup(galleryItems) {
       })
       .join('');
 }
+
+galeryContainer.addEventListener('click', onGalleryClick);
+
+function onEscPress(instance) {
+   return function (e) {
+      if (e.key === "Escape") {
+         instance.close();
+      }
+   }
+}
+
+function onGalleryClick(e) {
+   e.preventDefault();
+   const hasImageClass = e.target.classList.contains('gallery__image');
+   if (!hasImageClass) {
+      return;
+   }
+   const instance = basicLightbox.create(
+      `
+      <img src="${e.target.dataset.source}" width="800" height="600">
+      `,
+      {
+         onShow: instance => {
+            document.addEventListener("keydown", f);
+         },
+         onClose: instance => {
+            document.removeEventListener("keydown", f);
+         },
+      },
+   );
+   const f = onEscPress(instance);
+   instance.show();
+};
+
+
+
+
